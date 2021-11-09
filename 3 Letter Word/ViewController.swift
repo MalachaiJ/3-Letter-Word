59cd777eac767e48a0c50eeda6a5314e1fb214d7
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-11-09 10:47:18 -0700

    almost done

--- 3 Letter Word/ViewController.swift
+++ 3 Letter Word/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+// declare outlets and variables
+    
+    @IBOutlet weak var currentLetter: UILabel!
+    
+    @IBOutlet var lettersLabels: [UILabel]!
+    
+    @IBOutlet weak var horizStackView: UIStackView!
+    
+    var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var currentLetterA = "A"
+    var counter = 0
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func whenTapped(_ sender: UITapGestureRecognizer)
+    {
+        currentLetterA = letters[counter]
+        currentLetter.text = currentLetterA
+    }
+    
+    
+    
+    
 }
 
